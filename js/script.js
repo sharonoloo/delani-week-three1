adc3b254218a2e0cf00d8e353717471d58ef990e
sharon oloo <oloosharon@gmail.com>
2022-11-12 00:16:21 +0300

    Add css file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-jQuery('h1').click(function(){
-    alert("This is a heading tag")
+$(document).ready(function() {
+    $("h1").click(function() {
+      alert("This is a header.");
+    });
+  
+    $("p").click(function() {
+      alert("This is a paragraph.");
+    });
+  
+    $("img").click(function() {
+      alert("This is an image.");
+    });
   });
-
-  jQuery('p').click(function(){
-    alert("This is a paragraph")
-  });
-
-  jQuery('img').click(function(){
-    alert("This is an image")
-  });
+  
